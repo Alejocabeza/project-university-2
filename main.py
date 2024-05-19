61283d1c028376fe6eba9031c00e0d56230b860b
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2024-05-19 01:36:22 -0400

    add new features

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import tkinter as tk
-import utils.util_window as util_window
+import app
 
-class Main(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.config_window()
-
-    def config_window(self):
-        self.title("Grupo Imnova - Código Fonte")
-        w, h = 1024, 600
-        util_window.window_center(self, w, h)
-
-app = Main()
+app = app.Main()
 app.mainloop()
